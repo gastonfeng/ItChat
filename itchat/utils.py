aedbcce7694dec388c7b7010d1a8f28fb7e425b2
gastonfeng <gastonfeng@gmail.com>
2017-08-25 14:13:22 +0800

    修复CX_freeze打包后stdout丢失

--- itchat/utils.py
+++ itchat/utils.py
@@ -1,4 +1,10 @@
-import re, os, sys, subprocess, copy, traceback, logging
+import copy
+import logging
+import os
+import re
+import subprocess
+import sys
+import traceback
 
 try:
     from HTMLParser import HTMLParser
@@ -17,6 +23,28 @@ try:
     b = u'\u2588'
     sys.stdout.write(b + '\r')
     sys.stdout.flush()
+except IOError:
+    class dummyStream:
+        ''' dummyStream behaves like a stream but does nothing. '''
+
+        def __init__(self): pass
+
+        def write(self, data): pass
+
+        def read(self, data): pass
+
+        def flush(self): pass
+
+        def close(self): pass
+
+
+    # and now redirect all default streams to this dummyStream:
+    sys.stdout = dummyStream()
+    sys.stderr = dummyStream()
+    sys.stdin = dummyStream()
+    sys.__stdout__ = dummyStream()
+    sys.__stderr__ = dummyStream()
+    sys.__stdin__ = dummyStream()
 except UnicodeEncodeError:
     BLOCK = 'MM'
 else:
